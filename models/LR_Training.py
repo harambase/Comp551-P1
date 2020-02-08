1ebe0153609fe91438e87d0497893a81159da37b
linsh <lin.shilei@outlook.com>
2020-02-07 20:25:54 -0500

    Init Commit, Push the datasets

--- models/LR_Training.py
+++ models/LR_Training.py
@@ -7,21 +7,19 @@ import time
 
 
 def create_df_ionosphere(file):
-    pd.set_option('display.max_columns', 35)
-
     dataset = pd.DataFrame(pd.read_csv(file, header=None))
-
     dataset.replace('g', 1, inplace=True)
     dataset.replace('b', 0, inplace=True)
-
     return dataset
 
 
 def create_df_haberman(file):
-    pd.set_option('display.max_columns', 35)
-
     dataset = pd.DataFrame(pd.read_csv(file, header=None))
+    return dataset
 
+
+def create_df_heart(file):
+    dataset = pd.DataFrame(pd.read_csv(file, header=None, delim_whitespace=True))
     return dataset
 
 
@@ -31,6 +29,7 @@ def create_df_adult(file):
 
     input_array = input_array.drop(input_array.loc[:, 1:3], axis=1)
     input_array = input_array.drop(input_array.loc[:, 103:104], axis=1)
+
     input_array = (input_array - input_array.min()) / (input_array.max() - input_array.min())
     output_array = pd.DataFrame({'105': data[1]}).astype(float)
     df = pd.concat([input_array, output_array], axis=1)
@@ -68,45 +67,34 @@ def k_fold(df, model, k, rate, iteration, category):
 def train_and_predict_ionosphere():
     # Read Data
     df = create_df_ionosphere("../data/ionosphere.data")
-
-    # Logistic regression
-    lr = LogisticRegression(np.zeros((1, 34), float))
-    rate = .0005
-    iteration = 10
-
-    start_time = time.time()
-
-    result = k_fold(df, lr, 5, rate, iteration, [0, 1])
-
-    end_time = time.time()
-    print("Elapsed time for Logistic Regression on the ionosphere set is %g seconds" % (end_time - start_time))
-    print("The accuracy is %g" % result[0])
-
-    # Plot number of iterations vs Cost
-    plt.plot(result[1], '-y', label='a = ' + str(rate))
-    plt.xlabel("Iterations")
-    plt.ylabel("Cost")
-    plt.title("Number of Iterations vs Cost")
-    plt.legend(loc='upper right')
-    plt.tight_layout()
-    plt.show()
+    train_and_predict_by_k_fold(df, 'ionosphere')
 
 
 def train_and_predict_haberman():
     # Read Data
     df = create_df_ionosphere("../data/haberman.data")
+    train_and_predict_by_k_fold(df, 'haberman')
 
+
+def train_and_predict_heart():
+    # Read Data
+    df = create_df_heart("../data/heart.data")
+    train_and_predict_by_k_fold(df, 'heart')
+
+
+def train_and_predict_by_k_fold(df, name):
     # Logistic regression
-    lr = LogisticRegression(np.zeros((1, len(df.columns)-1), float))
-    rate = .0005
-    iteration = 50
+    lr = LogisticRegression(np.zeros((1, len(df.columns) - 1), float))
+    rate = .005
+    iteration = 100
 
     start_time = time.time()
 
+    print(df)
     result = k_fold(df, lr, 5, rate, iteration, [1, 2])
 
     end_time = time.time()
-    print("Elapsed time for Logistic Regression on the ionosphere set is %g seconds" % (end_time - start_time))
+    print("Elapsed time for Logistic Regression on the " + name + " set is %g seconds" % (end_time - start_time))
     print("The accuracy is %g" % result[0])
 
     # Plot number of iterations vs Cost
@@ -132,8 +120,6 @@ def train_and_predict_adult():
     start_time = time.time()
 
     df = df.iloc[np.random.permutation(len(df))]
-
-    costList = []
 
     X = df.iloc[:, 0:lens - 1]
     Y = df.iloc[:, lens - 1:lens]
@@ -162,5 +148,5 @@ def train_and_predict_adult():
     plt.show()
 
 
-train_and_predict_haberman()
-#train_and_predict_adult()
+train_and_predict_ionosphere()
+# train_and_predict_adult()
